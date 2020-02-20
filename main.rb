1f7635866eb0157cc6b647da60fe6e814637eac0
gustavoarildo <gustavoarildo@gmail.com>
2020-02-20 13:23:47 -0300

    primeira versao do jogo correcao

--- main.rb
+++ main.rb
@@ -1,7 +1,14 @@
-puts "Entre com um palpite :"
-palpite = gets.chomp.to_i
-
+puts "Bem-vindo ao jogo da adivinhacao"
+puts "Qual e o seu nome?"
+nome = gets
+puts "\n\n\n\n"
+puts "Comecaremos o jogo para voce, " + nome
+puts "Escolhendo um numero secreto entre 0 e 200..."
 numero_secreto = 121
-
-puts "Você acertou?"
-puts palpite == numero_secreto
+puts "Escolhido... que tal adivinhar hoje o nosso numero secreto?"
+puts "\n\n\n"
+puts "Tentativa 1"
+puts "Entre com o numero"
+chute = gets
+puts "Sera que acertou? Voce chutou " + chute
+puts numero_secreto == chute.to_i
