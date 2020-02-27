1d000fefeaa7f841955aeefbd1e714b53f1762b2
gustavoarildo <gustavoarildo@gmail.com>
2020-02-27 13:17:40 -0300

    #n4: pergunta se jogador quer continuar.

--- main.rb
+++ main.rb
@@ -8,27 +8,37 @@ numero_secreto = 121
 puts "Escolhido... que tal adivinhar hoje o nosso numero secreto?"
 puts "\n\n\n"
 $tentativas = 1
+$chute = 0
+continuar_jogo = "s"
 
 def pergunta!(numero_secreto)
-  chute = 0
-  if numero_secreto != chute.to_i
+  if numero_secreto != $chute.to_i
     puts "Tentativa " + $tentativas.to_s
     $tentativas = $tentativas + 1
+    if $tentativas > 3
+        $tentativas = 1
+    end
     puts "Entre com o numero"
-    chute = gets
-    puts "Sera que acertou? Voce chutou " + chute
-    puts numero_secreto == chute.to_i
+    $chute = gets
+    puts "Sera que acertou? Voce chutou " + $chute
+    puts numero_secreto == $chute.to_i
+
+    if numero_secreto == $chute.to_i
+        puts "você acertou parabens"
+      elsif numero_secreto > $chute.to_i
+        puts "você chutou um valor menor que o numero secreto"
+      else
+        puts "você chutou um valor maior que o numero secreto"
+    end
+      puts "\n\n\n"
   end
-  if numero_secreto == chute.to_i
-    puts "você acertou parabens"
-  elsif numero_secreto > chute.to_i
-    puts "você chutou um valor menor que o numero secreto"
-  else
-    puts "você chutou um valor maior que o numero secreto"
-  end
-  puts "\n\n\n"
 end
 
-3.times do
+while continuar_jogo == "s"
+  3.times do
     pergunta!(numero_secreto)
   end
+  puts nome + "quer continuar a jogar? (s/n)..."
+  continuar_jogo = gets.chomp
+  $chute = 0
+end
