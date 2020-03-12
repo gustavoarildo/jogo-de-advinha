e5775dbee81fdaf155419f167a5a5816e6be9850
gustavoarildo <gustavoarildo@gmail.com>
2020-03-12 10:36:08 -0300

    #5: lab04-solucao.

--- main.rb
+++ main.rb
@@ -1,44 +1,88 @@
-puts "Bem-vindo ao jogo da adivinhacao"
-puts "Qual e o seu nome?"
-nome = gets
-puts "\n\n\n\n"
-puts "Comecaremos o jogo para voce, " + nome
-puts "Escolhendo um numero secreto entre 0 e 200..."
-numero_secreto = 121
-puts "Escolhido... que tal adivinhar hoje o nosso numero secreto?"
-puts "\n\n\n"
-$tentativas = 1
-$chute = 0
-continuar_jogo = "s"
+def da_boas_vindas
+  puts "Bem-vindo ao jogo da adivinhacao"
+  puts "Qual e o seu nome?"
+  nome = gets
+  puts "\n\n\n\n"
+  puts "Comecaremos o jogo para voce, #{nome}"
+end
 
-def pergunta!(numero_secreto)
-  if numero_secreto != $chute.to_i
-    puts "Tentativa " + $tentativas.to_s
-    $tentativas = $tentativas + 1
-    if $tentativas > 3
-        $tentativas = 1
-    end
-    puts "Entre com o numero"
-    $chute = gets
-    puts "Sera que acertou? Voce chutou " + $chute
-    puts numero_secreto == $chute.to_i
+def pede_dificuldade
+  puts "Qual o nivel de dificuldade que deseja? (1 facil, 5 dificil) "
+  dificuldade = gets.to_i
+end
 
-    if numero_secreto == $chute.to_i
-        puts "você acertou parabens"
-      elsif numero_secreto > $chute.to_i
-        puts "você chutou um valor menor que o numero secreto"
-      else
-        puts "você chutou um valor maior que o numero secreto"
+def sorteia_numero_secreto (dificuldade)
+  case dificuldade
+  when 1
+    maximo = 30
+  when 2
+    maximo = 60
+  when 3
+    maximo = 100
+  when 4
+    maximo = 150
+  else
+    maximo = 200
+  end
+
+  puts "Escolhendo um numero secreto entre 0 e #{maximo}..."
+  numero_secreto = rand(maximo) + 1
+  puts "Escolhido... que tal adivinhar hoje o nosso numero secreto?"
+
+  numero_secreto
+end
+
+def pede_um_numero(chutes, tentativa, limite_de_tentativas)
+  puts "\n\n\n"
+  puts "Tentativa #{tentativa} de #{limite_de_tentativas}"
+  puts "Chutes ate agora #{chutes}"
+  puts "Entre com o numero"
+  chute = gets.strip.to_i
+  loop do
+    break unless chutes.include? chute
+    puts "Atencao, voce ja jogou o numero #{chute}!"
+    chute = gets.strip.to_i
+  end
+  puts "Sera que acertou? Voce chutou #{chute}"
+  chute
+end
+
+def verifica_se_acertou(numero_secreto, chute)
+  acertou = numero_secreto == chute
+  if acertou
+    puts "Acertou!"
+    return true
+  else
+    maior = numero_secreto > chute
+    if maior
+      puts "O numero secreto e maior!"
+    else
+      puts "O numero secreto e menor!"
     end
-      puts "\n\n\n"
+    false
   end
 end
 
-while continuar_jogo == "s"
-  3.times do
-    pergunta!(numero_secreto)
+def joga(limite_de_tentativas, dificuldade)
+  chutes = []
+  numero_secreto = sorteia_numero_secreto dificuldade
+  (1..limite_de_tentativas).each do |tentativa|
+    chute = pede_um_numero chutes, tentativa, limite_de_tentativas
+    chutes << chute
+    break if verifica_se_acertou numero_secreto, chute
   end
-  puts nome + "quer continuar a jogar? (s/n)..."
-  continuar_jogo = gets.chomp
-  $chute = 0
+end
+
+def nao_quer_jogar?
+  puts "Deseja jogar novamente? (S/N)"
+  quero_jogar = gets.strip.upcase
+  quero_jogar.casecmp("N").zero?
+end
+
+da_boas_vindas
+limite_de_tentativas = 3
+dificuldade = pede_dificuldade
+loop do
+  joga limite_de_tentativas, dificuldade
+  break if nao_quer_jogar?
 end
